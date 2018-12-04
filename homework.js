7d957a6e72f9a3a241babd8c59591a6177704de7
deannamjacobs <jacobs.deanna.m@gmail.com>
2018-12-04 09:38:12 -0500

    submit homework

--- homework.js
+++ homework.js
@@ -1,26 +1,54 @@
 // 1. Set the variable `name` to the string "Addison".
-
+let givenName = "Addison"
 // 2. Set candies equal to 20, people to 6, and leftover equal
 // to the remainder of dividing 20 by 6.
-
+let candies = 20
+let people = 6
+let leftover = 2
 // 3. Create a function called greeting that returns "Hello, <name>!",
 // where <name> is the name given as an argument to the function.
-
+function greeting(name) {
+    return `Hello, ${name}!`
+}
 // 4. Create a function called isOdd that, given a number, will
 // return true if it is odd and false if it is not. An odd number is a
 // number which, when divided by 2, has a remainder of 1 or -1.
-
+function isOdd(value) {
+    if (value % 2 === 1) {
+        return true
+    }
+    if (value % 2 === -1) {
+        return true
+    }
+    else {
+        return false
+    }
+}
 // 5. Create a function called isEven that, given a number, will
 // return true if it is even and false if it is not. An even number is a
 // number which, when divided by 2, has a remainder of 0.
+function isEven(value) {
+    if (value % 2 === 0) {
+        return true
+    }
+    else {
+        return false
+    }
+}
 
 // 6. Create a function called fahrenheitToCelsius that takes a
 // Fahrenheit temperature as an argument and returns the
 // temperature in Celsius.
+function fahrenheitToCelsius(value) {
+    return (value - 32) * (5 / 9)
+}
 
 // 6. Create a function called celsiusToFahrenheit that takes a
 // Celsius temperature as an argument and returns the
 // temperature in Fahrenheit.
+function celsiusToFahrenheit(value) {
+    return (value * 1.8) + (32)
+}
 
 // 7. Create a function called fahrenheitToKelvin that takes a
 // Fahrenheit temperature as an argument and returns the
@@ -28,10 +56,20 @@
 // fahrenheitToCelsius function.
 // Absolute zero (0 K) is equivalent to −273.15 C.
 // 1 degree Kelvin equals 1 degree Celsius.
-
+function fahrenheitToKelvin(value) {
+    return (value - 32) * 5 / 9 + 273.15
+}
 // 8. Create a function called lesser that takes two numbers as
 // arguments and returns the lesser of them. This function should
 // use an if/else statement.
+function lesser(x, y) {
+    if (x < y) {
+        return x
+    }
+    else {
+        return y
+    }
+}
 
 // 9. Create a function called multigreeting that takes a name
 // and a language code and returns a version of "Hello, <name>!"
@@ -44,15 +82,59 @@
 // eo - Saluton, <name>!
 //
 // If any other language code is used, return nothing.
-
+function multigreeting(name, languageCode) {
+    if (languageCode === "en") {
+        return `Hello, ${name}!`
+    }
+    if (languageCode === "es") {
+        return `¡Hola, ${name}!`
+    }
+    if (languageCode === "fr") {
+        return `Bonjour, ${name}!`
+    }
+    if (languageCode === "eo") {
+        return `Saluton, ${name}!`
+    }
+}
 // 10. Create a function called "sum" that takes an array of numbers and
 // returns the sum of those numbers.
+function sum(array) {
+    for (
+        var
+        index = 0,
+        length = array.length,
+        sum = 0;
+        index < length;
+        sum += array[index++]
+    );
+    return sum;
+}
 
 // 11. Create a function called "average" that takes an array of numbers
 // and returns the average of those numbers.
+function average(array) {
+    for (
+        var
+        index = 0,
+        length = array.length,
+        sum = 0;
+        index < length;
+        sum += array[index++]
+    );
+    var avg = sum / array.length
+}
+
 
 // 12. Create a function called "minimum" that takes an array of numbers and
 // returns the smallest number in that array.
+function minimum(array) {
+    array.min = function (array) {
+        return Math.min.apply(Math, array);
+    };
+    var minimum = array.min(array);
+
+
+}
 
 // 13. There are many techniques to sort arrays in programming. Your programming
 // language will likely include the ability to do this. We are going to
